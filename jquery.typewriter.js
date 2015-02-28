6c386b5a761f82131cea9c6f0165b2abcc3f858b
Allison Tarr <allison@createpositivespin.com>
2015-02-28 18:49:33 -0500

    Added sound effect file and blinking cursor

--- jquery.typewriter.js
+++ jquery.typewriter.js
@@ -10,8 +10,9 @@ $.fn.typewriter = function() {
 
 		// Clears text in the user selected element
 		$(this).text('');
-
-		//Show element, remove class .typeAction??
+		// Adds static cursor to user selected element
+		$(this).append($('<span>').text('|'));
+		//Show element, remove class .typeAction
 		$(this).removeClass('typeAction');
 
 		// Create variable to measure element length 
@@ -28,23 +29,57 @@ $.fn.typewriter = function() {
 		var position = 0;
 		// Set up an interval to loop every however many milliseconds
 		var type = setInterval(function() {
-			// We append the element to the user selected element
-			// $self refers to what element the user called our plugin on
-			// We use the position number to access the element from the array, like elementSplit[0]
-			$self.append(elementSplit[position]);
-			// Increment the position for the next interval we get the next element from our array
-			position++;
-			// If the position is greater the length of the array
-			if(position > elementLength) {
+
+			// Defines a random time interval
+			var randTime = (Math.random() * 1000); // 5 seconds in millisconds
+			  setTimeout(function() {
+			    console.log("Ran after ", randTime);
+
+			    // Sound effects
+					var audio = $('audio.sound')[0];
+					audio.currentTime = 0;
+					audio.play();
+
+      		// We append the element to the user selected element
+					// $self refers to what element the user called our plugin on
+					// We use the position number to access the element from the array, like elementSplit[0]
+			    $self.find('span').before(elementSplit[position]);
+			    // Increment the position for the next interval we get the next element from our array
+					position++;
+				}, randTime);
+
+			 // If the position is greater the length of the array
+			if (position > elementLength) {
 				// Shut it down
 				clearInterval(type);
 			}
-			// The number below is the milliseconds of our interval
-		},250);
+		},500); // ends setInterval
+
+			// We append the element to the user selected element
+			// $self refers to what element the user called our plugin on
+			// We use the position number to access the element from the array, like elementSplit[0]
+			// $self.find('span').before(elementSplit[position]);
+
+			// var audio = $('audio.sound')[0];
+   //    audio.play();
+
+			// Increment the position for the next interval we get the next element from our array
+			// position++;
+			// // If the position is greater the length of the array
+			// if(position > elementLength) {
+			// 	// Shut it down
+			// 	clearInterval(type);
+			// }
+
+			// Sets timeout using random time interval
+		// 	setTimeout(function() {
+  //   		console.log("Ran after ", randTime);
+  // 		}, randTime);
+		// },500); // ends setInterval
 
 };  // ends fn.typewriter
 
 // POTENTIAL CUSTOMIZATION OPTIONS
+
+// Default - typingSpeed : 250, cursor : boolean?, sound effects : boolean?
 // What happens when it reaches the end of element?
-// Typing speed, cursor showing, consistency of typing
-// Sound effects of typewriter
